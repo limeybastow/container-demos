bc81e04358f9cf955b3c03221ad45262442fde3b
John Bastow <jbastow@harbourvest.com>
2019-12-12 13:17:04 -0500

    More stuff.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.ColoredConsole()
+                .WriteTo.File("consoleapp.log")
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+        }
+    }
+}
