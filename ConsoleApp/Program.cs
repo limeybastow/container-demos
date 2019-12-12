d8384ab4969c3281c1cf8dbe1b28c1c022679d69
John Bastow <jbastow@harbourvest.com>
2019-12-12 18:02:13 -0500

    Finished demos.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -9,12 +10,43 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.ColoredConsole()
-                .WriteTo.File("consoleapp.log")
-                .CreateLogger();
+            LoggerConfiguration loggerConfiguration = new LoggerConfiguration()
+                .WriteTo.ColoredConsole();
 
-            Log.Information("Hello, world!");
+            string seqUrl = Environment.GetEnvironmentVariable("SEQ_URL");
+            Console.Write($"SEQ_URL - {seqUrl}");
+
+            if (!string.IsNullOrWhiteSpace(seqUrl))
+            {
+                loggerConfiguration.WriteTo.Seq(seqUrl);
+            }
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            int interation = 0;
+            Random randomGenerator = new Random();
+
+            try
+            {
+                while (true)
+                {
+                    int value = randomGenerator.Next(0, 10);
+
+                    if ((value % 5) == 0) Log.Warning("Value is divisible by 5!");
+
+                    Log.Information($"Iteration {interation++} - {value}");
+
+                    var someValue = 10 / value;
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal($"Oops!  Something broke!\r\n\r\n{ex.ToString()}");
+                Log.CloseAndFlush();
+                throw ex;
+            }
         }
     }
 }
